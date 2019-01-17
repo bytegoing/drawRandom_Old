61ccb82a8289198c77c595731bbb509f25ae1e93
BYTEGOING <37968741+bytegoing@users.noreply.github.com>
2019-01-17 22:45:12 +0800

    First commit
    Yeah, C#!

---
+++ drawRandom/main.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace drawRandom
+{
+    partial class main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.drawedLabel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.draw = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("黑体", 40F);
+            this.button1.Location = new System.Drawing.Point(12, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(360, 71);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "抽 签";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("黑体", 15F);
+            this.button2.Location = new System.Drawing.Point(12, 195);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(175, 62);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "清 除 已 抽";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("黑体", 15F);
+            this.label1.Location = new System.Drawing.Point(16, 273);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "共有";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("黑体", 15F);
+            this.label2.Location = new System.Drawing.Point(101, 273);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "人";
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.Font = new System.Drawing.Font("黑体", 15F);
+            this.totalLabel.Location = new System.Drawing.Point(64, 273);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(39, 20);
+            this.totalLabel.TabIndex = 4;
+            this.totalLabel.Text = "000";
+            // 
+            // drawedLabel
+            // 
+            this.drawedLabel.AutoSize = true;
+            this.drawedLabel.Font = new System.Drawing.Font("黑体", 15F);
+            this.drawedLabel.Location = new System.Drawing.Point(203, 273);
+            this.drawedLabel.Name = "drawedLabel";
+            this.drawedLabel.Size = new System.Drawing.Size(39, 20);
+            this.drawedLabel.TabIndex = 7;
+            this.drawedLabel.Text = "000";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("黑体", 15F);
+            this.label4.Location = new System.Drawing.Point(240, 273);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "人";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("黑体", 15F);
+            this.label5.Location = new System.Drawing.Point(137, 273);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "已抽取";
+            // 
+            // draw
+            // 
+            this.draw.AutoSize = true;
+            this.draw.Font = new System.Drawing.Font("黑体", 68F, System.Drawing.FontStyle.Bold);
+            this.draw.Location = new System.Drawing.Point(-12, 11);
+            this.draw.Name = "draw";
+            this.draw.Size = new System.Drawing.Size(411, 91);
+            this.draw.TabIndex = 8;
+            this.draw.Text = "正在处理";
+            this.draw.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("黑体", 15F);
+            this.button3.Location = new System.Drawing.Point(193, 195);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(179, 62);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "保 存 并 退 出";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(333, 270);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(39, 25);
+            this.button12.TabIndex = 20;
+            this.button12.Text = "关于";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(140, 20);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(39, 37);
+            this.button7.TabIndex = 13;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(185, 20);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(39, 37);
+            this.button8.TabIndex = 14;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(95, 20);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(39, 37);
+            this.button6.TabIndex = 12;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(230, 20);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(39, 37);
+            this.button9.TabIndex = 15;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(50, 20);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(39, 37);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(275, 20);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(39, 37);
+            this.button10.TabIndex = 16;
+            this.button10.Text = "button10";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(5, 20);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(39, 37);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(318, 20);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(39, 37);
+            this.button11.TabIndex = 17;
+            this.button11.Text = "button11";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button11);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button10);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button9);
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Controls.Add(this.button8);
+            this.groupBox1.Controls.Add(this.button7);
+            this.groupBox1.Location = new System.Drawing.Point(9, 304);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(363, 69);
+            this.groupBox1.TabIndex = 19;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "保存进度:";
+            this.groupBox1.Visible = false;
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 381);
+            this.ControlBox = false;
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.draw);
+            this.Controls.Add(this.drawedLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "main";
+            this.Text = "drawRandom";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Label drawedLabel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label draw;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ drawRandom/main.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace drawRandom
+{
+    public partial class main : Form
+    {
+        private string ver = "v2.0beta";
+
+        private string nameFilePath = @"name.in";
+        private string saveFilePath = @"save.out";
+
+        private string[] name = new string[101];
+        private bool[] drawed = new bool  [101];
+
+        private int totalNum = 1;
+        private int totalSave= 0;
+
+        private int randomNum()
+        {
+            return new Random(Guid.NewGuid().GetHashCode()).Next(1, totalNum);
+        }
+
+        private void clean()
+        {
+            draw.Text = "清理中...";
+            for (int i = 0; i <= 100; i++) drawed[i] = false;
+            totalSave = 0;
+            drawedLabel.Text = "0";
+            draw.Text = "清理完毕";
+            //MessageBox.Show("清理完毕","提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void save()
+        {
+            int saveItem = 0;
+            StreamWriter sw1 = new StreamWriter(saveFilePath);
+            for (int i = 1; i <= totalNum; i++)
+            {
+                if(drawed[i])
+                {
+                    saveItem++;
+                    sw1.WriteLine(i);
+                }
+            }
+            sw1.Close();
+            //MessageBox.Show("保存完毕,共保存"+saveItem+"人", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            draw.Text = "保存" + saveItem + "人";
+        }
+
+        public main()
+        {
+            InitializeComponent();
+            this.Text = "drawRandom " + ver;
+            for (int i = 0; i <= 100; i++) drawed[i] = false;
+            if (!File.Exists(nameFilePath))
+            {
+                MessageBox.Show("姓名文件不存在!请检查目录下是否存在name.in文件或检查程序是否有权限访问本目录文件!", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+
+            StreamReader nameStream = new StreamReader(nameFilePath, Encoding.Default);
+            string nextLine;
+            while( (nextLine = nameStream.ReadLine()) != null)
+            {
+                name[totalNum] = nextLine;
+                totalNum++;
+            }
+            nameStream.Close();
+            if(totalNum == 1)
+            {
+                MessageBox.Show("姓名文件为空!请编辑name.in来添加姓名,每行一个", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+            MessageBox.Show("导入成功!共导入" + (totalNum - 1) + "人", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            totalLabel.Text = (totalNum - 1) + "";
+
+            int nextDrawed = 0;
+            string tips = "[ ";
+            if (File.Exists(saveFilePath))
+            {
+                StreamReader saveStream = new StreamReader(saveFilePath, Encoding.Default);
+                while (!string.IsNullOrEmpty(nextLine = saveStream.ReadLine()))
+                {
+                    try
+                    {
+                        nextDrawed = int.Parse(nextLine);
+                    }
+                    catch (Exception e)
+                    {
+                        MessageBox.Show("进度文件损坏,内有非数字字符.清空进度...", "警告", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        totalSave = 0;
+                        clean();
+                        break;
+                    }
+                    drawed[nextDrawed] = true;
+                    totalSave++;
+                    tips += name[nextDrawed];
+                    tips += " ";
+                }
+                saveStream.Close();
+            }
+            tips += "]";
+            if (totalSave > 0)
+            {
+                MessageBox.Show("已经抽取" + totalSave + "人，分别为：" + tips, "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                draw.Text = nextDrawed + name[nextDrawed];
+            }
+            else
+            {
+                MessageBox.Show("抽签记录为空", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                draw.Text = "等待抽签";
+            }
+            drawedLabel.Text = totalSave + "";
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("drawRandom "+ver+"\nPowered By C#\n2019 BYTEGOING\nGithub: https://www.github.com/BYTEGOING", "关于", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            save();
+            DialogResult result = MessageBox.Show("确定要退出吗？", "drawRandom "+ver, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if(result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            draw.Text = "正在抽签";
+            int nextNum = randomNum();
+            while(drawed[nextNum])
+            {
+                nextNum = randomNum();
+            }
+            drawed[nextNum] = true;
+            //draw.Text = nextNum+name[nextNum];
+            draw.Text = name[nextNum];
+            totalSave++;
+            drawedLabel.Text = totalSave + "";
+            if (totalSave == totalNum - 1)
+            {
+                draw.Text = "抽取完毕";
+                MessageBox.Show("已经将所有人抽完,清理中...","提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                clean();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            clean();
+        }
+    }
+}
